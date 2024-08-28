d19c06e22012029d396f53477fce77eb6d319a03
nhatanhng <nguyennhatanh0810@gmail.com>
2024-08-28 16:39:35 +0700

    Update app.py
    create endpoints serving for choosing point id from csv file and return its corresponding data

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ import spectral as sp
 from npy_append_array import NpyAppendArray
 import numpy as np
 
+from pyproj import Proj, transform
+import math
+import utm
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -124,19 +127,112 @@ def hsi_to_rgb(hsi_img_name, red, green, blue):
 
     return output_path
 
+# def convert_northing_easting_to_lat_long(easting, northing, zone_number=48, northern_hemisphere=True):
+#     # Define the projection for UTM zone 48N
+#     proj_utm = Proj(proj='utm', zone=zone_number, ellps='WGS84', south=not northern_hemisphere)
+    
+#     # Define the projection for latitude and longitude
+#     proj_lat_long = Proj(proj='latlong', datum='WGS84')
+    
+#     # Convert easting and northing to latitude and longitude
+#     longitude, latitude = transform(proj_utm, proj_lat_long, easting, northing)
+    
+#     return latitude, longitude
+
+# # def latlon_to_pixels(latitude, longitude, map_width, map_height):
+#     # Convert longitude to x
+#     x = (longitude + 180) / 360 * map_width
+    
+#     # Convert latitude to y using the Mercator projection formula
+#     lat_rad = math.radians(latitude)
+#     merc_n = math.log(math.tan((math.pi / 4) + (lat_rad / 2)))
+#     y = (map_height / 2) - (map_width * merc_n / (2 * math.pi))
+    
+#     return int(x), int(y)
+
+# def calculate_pixel_coordinates(top_left_northing, top_left_easting, point_northing, point_easting, meters_per_pixel):
+#     # Calculate the difference in UTM coordinates
+#     delta_easting = point_easting - top_left_easting
+#     delta_northing = top_left_northing - point_northing  # Subtract because moving down
+    
+#     # Convert the UTM differences to pixel offsets
+#     pixel_x = int(delta_easting / meters_per_pixel)
+#     pixel_y = int(delta_northing / meters_per_pixel)
+    
+#     return pixel_x, pixel_y
+
+# def adjust_coordinates_to_fit(image_width, image_height, all_pixel_coords):
+#     min_x, min_y = float('inf'), float('inf')
+    
+#     # Find the minimum pixel coordinates to determine the offset needed
+#     for pixel_x, pixel_y in all_pixel_coords:
+#         min_x = min(min_x, pixel_x)
+#         min_y = min(min_y, pixel_y)
+    
+#     # Calculate the necessary offset to bring all coordinates within the image bounds
+#     x_offset = -min_x if min_x < 0 else 0
+#     y_offset = -min_y if min_y < 0 else 0
+    
+#     return x_offset, y_offset
+
+
 # def calculate_and_store_pixel_coordinates():
 #     base_point = StatisticalData.query.filter_by(point_id='BASE').first()
     
 #     if not base_point:
 #         print("BASE point not found.")
 #         return
+
+#     top_left_northing = base_point.y  # Assuming y represents northing in the database
+#     top_left_easting = base_point.x  # Assuming x represents easting in the database
     
+#     # Assume meters per pixel is known
+#     meters_per_pixel = 0.03  # Example resolution, adjust based on your data
+
 #     all_points = StatisticalData.query.filter(StatisticalData.point_id != 'BASE').all()
     
+#     all_pixel_coords = []
+
 #     for point in all_points:
-#         # Assign variables to store in db
-#         pixel_x = 
-#         pixel_y = 
+#         visualized_image = VisualizedImages.query.filter_by(id=point.image_id).first()
+#         image_width = visualized_image.width
+#         image_height = visualized_image.height
+
+#         # # Convert northing and easting to latitude and longitude
+#         # latitude, longitude = convert_northing_easting_to_lat_long(point.x, point.y)
+#         # print(point.x, point.y)
+
+#         # # Convert latitude and longitude to pixel coordinates
+#         # pixel_x, pixel_y = latlon_to_pixels(latitude, longitude, image_width, image_height)
+#         # print(pixel_x, pixel_y)
+
+#         # Convert point northing/easting to pixel coordinates
+#         pixel_x, pixel_y = calculate_pixel_coordinates(top_left_northing, top_left_easting, point.y, point.x, meters_per_pixel)
+#         all_pixel_coords.append((pixel_x, pixel_y))
+
+#     # Adjust all coordinates to fit within the image dimensions
+#     x_offset, y_offset = adjust_coordinates_to_fit(image_width, image_height, all_pixel_coords)
+   
+
+
+#     #     new_point = Points(
+#     #         image_id=point.image_id,
+#     #         point_id=point.point_id,
+#     #         x=pixel_x,
+#     #         y=pixel_y
+#     #     )
+        
+#     #     db.session.add(new_point)
+    
+#     # db.session.commit()
+
+#     # Store the adjusted coordinates in the database
+#     for i, point in enumerate(all_points):
+#         pixel_x, pixel_y = all_pixel_coords[i]
+        
+#         # Apply the offset
+#         pixel_x += x_offset
+#         pixel_y += y_offset
         
 #         new_point = Points(
 #             image_id=point.image_id,
@@ -335,62 +431,50 @@ def get_visualized_file(filename):
         logging.error(f"Error serving visualized file: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
-# @app.route('/save_points/<filename>', methods=['POST'])
-# def save_points(filename):
-#     try:
-#         file = Files.query.filter_by(filename=filename).first()
-#         if not file:
-#             return jsonify({"error": "File record not found"}), 404
-
-#         points = request.json.get('points', [])
-        
-#         Point.query.filter_by(file_id=file.id).delete()
-
-#         for point in points:
-#             new_point = Point(
-#                 file_id=file.id,
-#                 x=point['x'],
-#                 y=point['y']
-#             )
-#             db.session.add(new_point)
-
-#         db.session.commit()
-#         return jsonify({"message": "Points saved successfully"}), 200
-
-#     except Exception as e:
-#         logging.error(f"Error saving points: {str(e)}")
-#         return jsonify({"error": str(e)}), 500
-
-@app.route('/get_points/<filename>', methods=['GET'])
-def get_points(filename):
-    print(f"{filename}")
-    if filename.endswith('.img'):
-        filename = filename.replace('.img', '.png')
-    print(f"{filename}")    
-    
+@app.route('/save_points/<filename>', methods=['POST'])
+def save_points(filename):
     try:
-        image = VisualizedImages.query.filter_by(visualized_filename = filename).first()
-        if not image:
-            return jsonify({"error": "File record not found"}), 404
-        points = Points.query.filter_by(image_id=image.id).all()
-        return jsonify([{'x': point.x, 'y': point.y} for point in points]), 200
-    except Exception as e:
-        logging.error(f"Error fetching points: {str(e)}")
-        return jsonify({"error": str(e)}), 500
-    
-# @app.route('/delete_point/<int:point_id>', methods=['DELETE'])
-# def delete_point(point_id):
-#     try:
-#         point = Point.query.get(point_id)
-#         if not point:
-#             return jsonify({"error": "Point not found"}), 404
+        if not filename.endswith('.png'):
+            filename = filename.rsplit('.', 1)[0] + '.png'
 
-#         db.session.delete(point)
-#         db.session.commit()
-#         return jsonify({"message": "Point deleted successfully"}), 200
-#     except Exception as e:
-#         logging.error(f"Error deleting point: {str(e)}")
-#         return jsonify({"error": str(e)}), 500
+        print(filename)
+        visualized_image = VisualizedImages.query.filter_by(visualized_filename=filename).first()
+        if not visualized_image:
+            return jsonify({"error": "File record not found"}), 404
+
+        points = request.json.get('points', [])
+        
+        Points.query.filter_by(image_id=visualized_image.id).delete()
+
+        for point in points:
+            new_point = Points(
+                image_id=visualized_image.id,
+                x=point['x'],
+                y=point['y']
+            )
+            db.session.add(new_point)
+
+        db.session.commit()
+        return jsonify({"message": "Points saved successfully"}), 200
+
+    except Exception as e:
+        logging.error(f"Error saving points: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route('/delete_point/<int:point_id>', methods=['DELETE'])
+def delete_point(point_id):
+    try:
+        point = Points.query.get(point_id)
+        if not point:
+            return jsonify({"error": "Point not found"}), 404
+
+        db.session.delete(point)
+        db.session.commit()
+        return jsonify({"message": "Point deleted successfully"}), 200
+    except Exception as e:
+        logging.error(f"Error deleting point: {str(e)}")
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/upload_csv', methods=['POST'])
 def upload_csv():
@@ -398,7 +482,8 @@ def upload_csv():
         return jsonify({"error": "No file or image_id provided"}), 400
     
     file = request.files['file']
-    image_filename = request.form['image_id']  
+    image_filename = request.form['image_id'] 
+    print(image_filename) 
 
     if image_filename.endswith('.img'):
         image_filename = image_filename.replace('.img', '.png')
@@ -447,12 +532,12 @@ def upload_csv():
                 db.session.add(new_entry)
 
             db.session.commit()
-            logging.info("CSV data uploaded and added successfully.")
+            # logging.info("CSV data uploaded and added successfully.")
 
             # calculate_and_store_pixel_coordinates()
             # logging.info("pixel coordinated calculated and stored.")
 
-            return jsonify({"message": "all process successfully."}), 200
+            return jsonify({"message": "CSV data uploaded and added successfully."}), 200
 
         except Exception as e:
             logging.error(f"Error processing CSV: {e}")
@@ -460,6 +545,151 @@ def upload_csv():
             return jsonify({"error": "An error occurred while processing the CSV file."}), 500
 
     return jsonify({"error": "Invalid file format"}), 400
+
+@app.route('/get_point_ids', methods=['GET'])
+def get_point_ids():
+    try:
+        # Retrieve all unique point IDs from the StatisticalData table
+        point_ids = db.session.query(StatisticalData.point_id).distinct().all()
+
+        # Flatten the list of tuples
+        point_ids = [point_id[0] for point_id in point_ids]
+
+        return jsonify({"point_ids": point_ids}), 200
+
+    except Exception as e:
+        logging.error(f"Error retrieving point IDs: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/store_point_id', methods=['POST'])
+def store_point_id():
+    try:
+        # Get the selected point details from the request
+        selected_point = request.json.get('selected_point', {})
+        print(f"Received selected_point: {selected_point}")
+        
+        point_id = selected_point.get('point_id')
+        x = selected_point.get('x')
+        y = selected_point.get('y')
+        image_id = selected_point.get('image_id')
+
+        print(f"Extracted point_id: {point_id}, x: {x}, y: {y}, image_id: {image_id}")
+
+        if not point_id:
+            return jsonify({"error": "No point_id provided"}), 400
+
+        # Resolve the image_id from the image filename
+        visualized_image = VisualizedImages.query.filter_by(visualized_filename=image_id.replace('.img', '.png')).first()
+
+        if not visualized_image:
+            print(f"No VisualizedImages entry found for visualized_filename: {image_id}")
+            return jsonify({"error": "Invalid image_id provided"}), 400
+        
+        image_id_num = visualized_image.id
+        print(f"Resolved image_id_num: {image_id_num}")
+
+        # Find the corresponding point in the Points table
+        existing_point = Points.query.filter_by(
+            image_id=image_id_num,
+            x=x,
+            y=y
+        ).first()
+
+        if existing_point:
+            # Update the point_id if necessary
+            existing_point.point_id = point_id
+            print(f"Updated existing Point: {existing_point}")
+        else:
+            # Insert a new entry into the Points table
+            new_point = Points(
+                image_id=image_id_num,
+                point_id=point_id,
+                x=x,
+                y=y
+            )
+            db.session.add(new_point)
+            print(f"Added new Point: {new_point}")
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        return jsonify({"message": "Point ID stored successfully"}), 200
+
+    except Exception as e:
+        logging.error(f"Error storing point ID: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_statistical_data', methods=['POST'])
+def get_statistical_data():
+    try:
+        # Get the selected point_id from the request
+        point_id = request.json.get('point_id')
+        print(f"Received point_id: {point_id}")
+
+        if not point_id:
+            return jsonify({"error": "No point_id provided"}), 400
+
+        # Retrieve the StatisticalData entry that matches the provided point_id
+        statistical_data_entry = StatisticalData.query.filter_by(point_id=point_id).first()
+
+        if not statistical_data_entry:
+            print(f"No StatisticalData entry found for point_id: {point_id}")
+            return jsonify({"error": "No data found for the given point_id"}), 404
+
+        # Convert the data to a dictionary to return as JSON
+        data = {
+            "id": statistical_data_entry.id,
+            "image_id": statistical_data_entry.image_id,
+            "point_id": statistical_data_entry.point_id,
+            "x": statistical_data_entry.x,
+            "y": statistical_data_entry.y,
+            "h": statistical_data_entry.h,
+            "replicate": statistical_data_entry.replicate,
+            "sub_replicate": statistical_data_entry.sub_replicate,
+            "chlorophyll": statistical_data_entry.chlorophyll,
+            "rice_height": statistical_data_entry.rice_height,
+            "spectral_num": statistical_data_entry.spectral_num,
+            "digesion": statistical_data_entry.digesion,
+            "p_conc": statistical_data_entry.p_conc,
+            "k_conc": statistical_data_entry.k_conc,
+            "n_conc": statistical_data_entry.n_conc,
+            "chlorophyll_a": statistical_data_entry.chlorophyll_a,
+            "date": statistical_data_entry.date
+        }
+
+        print(f"Returning statistical data: {data}")
+        return jsonify({"statistical_data": data}), 200
+
+    except Exception as e:
+        logging.error(f"Error retrieving statistical data: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_points/<image_filename>', methods=['GET'])
+def get_points(image_filename):
+    try:
+        visualized_image = VisualizedImages.query.filter_by(visualized_filename=image_filename).first()
+
+        if not visualized_image:
+            return jsonify({"error": "Image not found"}), 404
+
+        points = Points.query.filter_by(image_id=visualized_image.id).all()
+
+        points_data = [
+            {
+                "id": point.id,
+                "x": point.x,
+                "y": point.y,
+                "point_id": point.point_id
+            }
+            for point in points
+        ]
+
+        return jsonify(points_data), 200
+
+    except Exception as e:
+        logging.error(f"Error retrieving points: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
